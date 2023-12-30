5062c9d69c637696f0474d13cc846295438c79c2
Thanawat Jarusuthirug <lynniswaifu@gmail.com>
2023-12-30 18:53:36 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
-setuptools.setup(
+setup(
     name="anipie", 
-    version="{{VERSION_PLACEHOLDER}}",
+    version='0.0.7',
     author="Aritsu",
     author_email="lynniswaifu@gmail.com",
     description="a simple python wrapper for the Anilist API",
     long_description="",
     long_description_content_type="text/markdown",
     url="",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
